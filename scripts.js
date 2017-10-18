e94cc74be445e644abef729d904feb342b9bd4c8
Manokero <dancebreak29@gmail.com>
2017-10-17 21:10:45 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,26 +5,24 @@ function Paint(x,y,color){
   this.ctx = this.canvas.getContext("2d");
   //this.color = "red";
   this.size = document.getElementById("size").value;
-  
   this.draw = function(){
-  this.ctx.fillStyle = color;  this.ctx.fillRect(this.x,this.y,this.size,this.size)  
+  this.ctx.fillStyle = color;  this.ctx.fillRect(
+    this.x,this.y,this.size,this.size
+    )  
   }
 }
 var paint;
 var color = "transparent";
 function move(event){
- 
   var x = event.clientX;
   var y = event.clientY;
   paint = new Paint(x,y,color)
   //this.color=color;
   paint.draw()
 }
-
 function begin(){
-  color= document.getElementById("color").value
+  color = document.getElementById("color").value
 }
-
 function end(){
   color="transparent"
 }
